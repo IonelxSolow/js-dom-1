899a9e6014aa5b619fe820ee963373b3c578a561
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-18 17:31:06 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const element = document.getElementById("lampadina");
+const button = document.getElementById("toggleButton")
+
+button.addEventListener("click", () => {
+    lampadina.src = "./assets/img/yellow_lamp.png";})
